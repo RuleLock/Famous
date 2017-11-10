1d0bab5bf1be5a97b3bb88b1e3e5c25ea9f6f437
fxc0719 <1@fxcdev.com>
2017-11-10 15:24:41 +0800

    add follow

---
+++ app/src/main/java/com/locker/famous/follow/FollowPeoples.kt
@@ -0,0 +1,20 @@
+package com.locker.famous.follow
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+/**
+ * @description
+ * @author fxc
+ * @date 2017/11/10.
+ */
+class FollowPeoples : AppCompatActivity() {
+    val TAG = "FollowPeoples"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate")
+
+    }
+
+}
